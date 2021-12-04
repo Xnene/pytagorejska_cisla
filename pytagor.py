af6b098b19891d4acdf379c8a441e4352f1570f7
Nene - desktop <mgros@netsecure.cz>
2021-12-04 12:32:18 +0100

    Update pytagor.py

--- pytagor.py
+++ pytagor.py
@@ -5,23 +5,37 @@
 import math
 import os
 
+#nejvetsi spolecny delitel 
+def gcd(a, b):
+    while (b != 0):
+        c = b
+        b = a % b
+        a = c
+  
+    return a
+
+
+
 tmp_c_multy = []
 tmp_pythagor =[]
 
 clear = lambda: os.system('cls')
 clear()
 
-for x in range(1, 101):
-    for y in range(1, 101): 
-        c = math.sqrt(x**2 + y**2)
+for a in range(1, 101):
+    for b in range(1, 101): 
+        c = math.sqrt(a**2 + b**2)
         if  float(c).is_integer():
            # print("{0} - {1}  = {2}".format(x,y,c))
-            res_pyth_combi_found = [x,y,int(c)]
+            res_pyth_combi_found = [a,b,int(c)]
             res_pyth_combi_found.sort() 
             if not ( res_pyth_combi_found in tmp_pythagor ):
                 if not c in tmp_c_multy:
-                    tmp_c_multy.append(int(c))
-                    tmp_pythagor.append(res_pyth_combi_found)
+                    # zobrazime si delitel
+                   if (gcd(a,c) == 1) and (gcd(b,c) == 1):
+                        print("founded pythagoras number \t "+str(res_pyth_combi_found)+ " delitel "  + str(gcd(a,c))+ "\t" + str(gcd(b,c)) )
+                        tmp_c_multy.append(int(c))
+                        tmp_pythagor.append(res_pyth_combi_found)
                     
             
 
@@ -31,7 +45,7 @@ for x in range(1, 101):
 tmp_c_multy.sort()
 
 result_pytagoras = []
-
+result_pytagoras = tmp_pythagor
                   
 #print(result_pytagoras)
 
