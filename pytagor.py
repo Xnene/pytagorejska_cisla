6669ebfecaf8e1c2162e16b538297602b2e3ffb8
Nene - desktop <mgros@netsecure.cz>
2021-12-05 23:07:49 +0100

    rename list refactoring

--- pytagor.py
+++ pytagor.py
@@ -25,9 +25,9 @@ def gcd(a, b):
 def search_pythagoras_numbers(max_a, max_b, basic = True):
 
     tmp_list_uniq_c = []
-    tmp_list_pythagoras_found_number =[]
-    tmp_list_pythagoras_found_basic_number =[]
-    tmp_list_pythagoras_found_withou_switch_number = []
+    tmp_list_pythagoras_found_numbers =[]
+    tmp_list_pythagoras_found_basic_numbers =[]
+    tmp_list_pythagoras_found_withou_switch_numbers = []
 
 
     for a in range(1, max_a+1):
@@ -37,36 +37,36 @@ def search_pythagoras_numbers(max_a, max_b, basic = True):
 
                 res_pyth_combi_found = [int(c),b,a]       
                 #test jedinečnosti 
-                if not ( res_pyth_combi_found in tmp_list_pythagoras_found_basic_number ):
+                if not ( res_pyth_combi_found in tmp_list_pythagoras_found_numbers ):
                     #show combination c , a , b  
                     #print("\t c: " +str(c)+"\t b: " +str(b)+"\t a: " +str(a))  
-                    tmp_list_pythagoras_found_number.append(res_pyth_combi_found)
+                    tmp_list_pythagoras_found_numbers.append(res_pyth_combi_found)
                     
                     # test na odstranení prehozených kombinace ab , ba 
                     if not c in tmp_list_uniq_c:
-                        tmp_list_pythagoras_found_withou_switch_number.append(res_pyth_combi_found)
+                        tmp_list_pythagoras_found_withou_switch_numbers.append(res_pyth_combi_found)
                         
                         #  Nalzezení základní pytagorijské trojice a zobrazime si divisor
                         if (gcd(a,c) == 1) and (gcd(b,c) == 1):
                             #print("found  basic pythagoras numbers \t "+str(res_pyth_combi_found)+ "\t divisor "  + str(gcd(a,c))+ "\t" + str(gcd(b,c)) )
                             tmp_list_uniq_c.append(int(c))
-                            tmp_list_pythagoras_found_basic_number.append(res_pyth_combi_found)
+                            tmp_list_pythagoras_found_basic_numbers.append(res_pyth_combi_found)
     if basic:
-        out = [tmp_list_pythagoras_found_basic_number]
+        out = [tmp_list_pythagoras_found_basic_numbers]
     else:
-        out = [tmp_list_pythagoras_found_basic_number,tmp_list_pythagoras_found_number,tmp_list_pythagoras_found_withou_switch_number]
+        out = [tmp_list_pythagoras_found_basic_numbers,tmp_list_pythagoras_found_numbers,tmp_list_pythagoras_found_withou_switch_numbers]
     
     return out                
 
 # nejake to zobrazeni             
-def show_list_number(list_of_number):
+def show_list_number(list_of_numbers):
     print("found following numbers in list ")
     j=1
-    for show_i in list_of_number:
+    for show_i in list_of_numbers:
         #print("found  basic pythagoras numbers \t "+str(res_pyth_combi_found)+ "\t divisor "  + str(gcd(a,c))+ "\t" + str(gcd(b,c)) )
         print(" " + str(j) + "\t "+str(show_i)+ "\t divisor "  + str(gcd(show_i[1],show_i[0])))
         j+= 1
-    print("Amount of numbers found " + str(len(list_of_number)))      
+    print("Amount of numbers found " + str(len(list_of_numbers)))      
         
 
 clearConsole()
